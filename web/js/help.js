4ea1b4dcdef673d4c85588b5c1c8d8a5f0687877
MIt_gancm <72749591+MIt-gancm@users.noreply.github.com>
2024-08-26 22:12:08 +0800

    更新帮助文档

--- web/js/help.js
+++ web/js/help.js
@@ -44,8 +44,57 @@ apt update -y & apt upgrade -y & git clone https://gitee.com/MIt-gancm/Autumn-le
 \`\`\`
 画饼~~（proot的有空换个位置存储方便一点）~~
 不行截图问群友或者临时会话私信发群主~~高中一个月拿一次手机~~看到会回~~几百年吧可能~~
+**请先查看:如何提供问题寻求帮助？&提问的智慧！**
 `},
 
+How_to_ask_a_question: {
+    title: "报错脚本出问题了？",
+    content: `
+# 发现错误 在错误界面精准截图
+发现错误不要慌：
+脚本执行带**警告**的部分截图
+脚本执行带**错误**的部分截图
+脚本执行带**报错日志**的部分截图
+单开一个终端:
+执行:
+\`\`\`shell
+uname -a
+# 设备基本信息   排除框架问题
+\`\`\`
+
+# 发现错误 精准描述拒绝无用信息
+在截图之后尽可能详细准确的描述你干了什么
+执行脚本的命令是什么
+在脚本里选择了什么
+您尽可能描述问题我们就可以更方便的找到错误并更正
+
+# 常见错误 _找不到xxx
+像截图找不到wget
+![这是图片](./images/not_fomund_xxx.png "找不到wget")
+运行:
+\`\`\`shell
+pkg install wget -y
+# termux
+apt install wget -y
+# Linux
+# 还不行!?
+apt remove wget -y & apt install wget -y
+pkg remove wget -y & apt install wget -y
+#wget 替换找不到的包的名字
+\`\`\`
+
+# 常见错误 _挂载不到桌面
+像截图找不到桌面
+![这是图片](./images/noX11.png "找不到桌面")
+运行:
+\`\`\`shell
+vim ~/.gancm/MCserver/核心种类/版本/start.sh
+# 在java命令后面加上 nogui
+# 编辑按 i 
+# 完成按 ESC 然后 : (必须是英文的冒号) wq (写入和退出) 回车完成
+\`\`\`
+**实在不行换核心**
+`}
     // 其他问题的内容...
 };
 
